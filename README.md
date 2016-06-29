188bae304bad56e07069de40f5d9ff922552449d
Constantin Titarenko <constantin.titarenko@gmail.com>
2016-06-29 17:34:22 +0300

    add ro, drop internal code validator

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # fix-phone
 
+[![Build Status](https://secure.travis-ci.org/titarenko/fix-phone.png?branch=master)](https://travis-ci.org/titarenko/fix-phone) [![Coverage Status](https://coveralls.io/repos/titarenko/fix-phone/badge.png)](https://coveralls.io/r/titarenko/fix-phone)
+
 Validate and format phone number, or return nothing, if such phone is not valid.
 
 ## Installation
 
 ```bash
-npm i fix-phone
+npm i fix-phone --save
 ```
 
 ## Usage
@@ -16,6 +18,13 @@ var fix = require('fix-phone');
 fix('ua', '050-121-22-33'); // returns +380501212233
 fix('ua', '010-121-22-33'); // returns null
 ```
+
+## Supported country codes
+
+- ua
+- ru
+- kz
+- ro
 
 ## License
 
--- index.js
+++ index.js
@@ -2,86 +2,37 @@ var _ = require('lodash');
 
 module.exports = fixPhone;
 
-function fixPhone (cc, phone, omitValidation) {
-	if (omitValidation) {
-		switch (cc) {
-			case 'ru': return fixRuPhoneNoValidation(phone);
-			case 'ua': return fixUaPhoneNoValidation(phone);
-			case 'kz': return fixKzPhoneNoValidation(phone);
-			default: return null;
-		}
-	}
+function fixPhone (cc, phone) {
 	switch (cc) {
 		case 'ru': return fixRuPhone(phone);
 		case 'ua': return fixUaPhone(phone);
 		case 'kz': return fixKzPhone(phone);
+		case 'ro': return fixRoPhone(phone);
 		default: return null;
 	}
 }
 
-var fixUaPhone = fixPhoneBuilder(8, 14, false, '+380', /^\+38(\d{3})/, 
-	[50, 63, 66, 67, 68, 73].concat(
-		_.range(90, 99 + 1), 
-		_.range(31, 38 + 1),
-		41,
-		_.range(43, 48 + 1),
-		_.range(51, 57 + 1),
-		_.range(61, 62 + 1),
-		64, 65, 69
- 	)
-);
+var fixUaPhone = fixPhoneBuilder(9, 13, false, '+380');
+var fixRuPhone = fixPhoneBuilder(10, 12, true, '+7');
+var fixKzPhone = fixPhoneBuilder(10, 12, true, '+7');
+var fixRoPhone = fixPhoneBuilder(9, 12, false, '+40');
 
-var fixUaPhoneNoValidation = fixPhoneBuilder(8, 14, false, '+380');
-
-var fixKzPhone = fixPhoneBuilder(9, 13, true, '+7', /^\+7(\d{3})/, 
-	[700, 701, 702, 705, 707, 708, 747, 771, 775, 776, 777, 778, 336].concat(
-		_.range(710, 718 + 1),
-		_.range(716, 718 + 1),
-		_.range(721, 729 + 1)
-	)
-);
-
-var fixKzPhoneNoValidation = fixPhoneBuilder(9, 13, true, '+7');
-
-var fixRuPhone = fixPhoneBuilder(9, 13, true, '+7', /^\+7(\d{3})/,
-	[].concat(
-		_.range(900, 906 + 1), _.range(908, 934 + 1), _.range(936, 941 + 1), _.range(950, 956 + 1), 958, _.range(960, 971 + 1), _.range(977, 978 + 1), _.range(980, 989 + 1), _.range(991, 997 + 1), 999,
-		_.range(301, 302 + 1), 336, _.range(341, 343 + 1), _.range(345, 347 + 1), 349, _.range(351, 353 + 1), 365, _.range(381, 385 + 1), 388, _.range(390, 391 + 1), _.range(394, 395 + 1),
-		401, 411, 413, _.range(415, 416 + 1), 421, _.range(423, 424 + 1), _.range(426, 427 + 1), _.range(471, 475 + 1), _.range(481, 487 + 1), _.range(491, 496 + 1), 499,
-		_.range(811, 818 + 1), _.range(820, 821 + 1), 831, _.range(833, 836 + 1), _.range(840, 848 + 1), 851, 855, _.range(861, 863 + 1), _.range(865, 867 + 1), 869, _.range(871, 873 + 1), _.range(877, 879 + 1)
-	)
-);
-
-var fixRuPhoneNoValidation = fixPhoneBuilder(9, 13, true, '+7');
-
-function fixPhoneBuilder (minLength, maxLength, replace8, prefix, codeRegexp, validCodes) {
+function fixPhoneBuilder (minLength, maxLength, replace8, prefix) {
 	return function (phone) {
 		phone = phone.replace(/[^\d\+]/g, '');
-		if (phone.length <= minLength || phone.length >= maxLength) {
+		if (phone.length < minLength || phone.length > maxLength) {
 			return null;
 		}
 
 		if (replace8 && phone.length > 10 && phone[0] == '8') {
-			phone = '+7' + phone.slice(1);
+			phone = prefix + phone.slice(1);
 		}
 
-		var offset = maxLength - 1 - phone.length;
+		var offset = maxLength - phone.length;
 		phone = prefix.slice(0, offset) + phone;
 
 		if (phone.slice(0, prefix.length) != prefix) {
 			return null;
-		}
-
-		if (_.isRegExp(codeRegexp) && !_.isEmpty(validCodes)) {
-			var operatorCode = phone.match(codeRegexp);
-			operatorCode = +(operatorCode && operatorCode[1]);
-			if (isNaN(operatorCode)) {
-				return null;
-			}
-
-			if (!_.contains(validCodes, operatorCode)) {
-				return null;
-			}
 		}
 
 		return phone;
