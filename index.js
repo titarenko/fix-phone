cc5e91801bbbfec9002e2f236749672830d20d8b
Yaroslav Udodov <yaroslav.udodov@akurateco.com>
2018-02-20 17:13:25 +0200

    CRM-2360

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ var countries = {
     countryCode: '+65',
     countryLocalPrefix: '',
     localCodeLength: 2,
-    phoneLength: 7,
+    phoneLength: 6,
     hasLocalPrefix: function (phone) {
       return phone.length >= 10 && phone[0] == '0'
     }
@@ -315,7 +315,7 @@ var fixEsPhone = fixPhoneBuilder(9, 12, 'es');
 var fixPtPhone = fixPhoneBuilder(9, 13, 'pt');
 var fixItPhone = fixPhoneBuilder(10, 13, 'it');
 var fixFrPhone = fixPhoneBuilder(9, 12, 'fr');
-var fixSgpPhone = fixPhoneBuilder(1, 14, 'sgp');
+var fixSgpPhone = fixPhoneBuilder(8, 11, 'sgp');
 var fixHuPhone = function (phone) {
 	phone = phone.replace(/^00/g, '')
 	
