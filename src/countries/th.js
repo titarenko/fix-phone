5dc618f482f6d7f7cee700c9c448fa0aa069b486
Yuriy Makov <juramak98@gmail.com>
2018-12-05 15:50:34 +0200

    th: removed fixing 8-digit number or number starting with zero and 8 digits after (#50)

--- src/countries/th.js
+++ src/countries/th.js
@@ -10,17 +10,25 @@ var config = {
   }
 }
 
-var fixThMobilePhone = tools.fixPhoneBuilder(8, 12, config);
-var fixThCityPhone = tools.fixPhoneBuilder(8, 11, config);
+var fixThMobilePhone = tools.fixPhoneBuilder(9, 12, config);
+var fixThCityPhone = tools.fixPhoneBuilder(9, 11, config);
 
 var fixThPhone = function (phone) {
   var localCode = tools.getLocalCode(config, phone)
   if ([2, 3, 4, 5, 7].indexOf(Number(localCode[0])) != -1) {
-    return fixThCityPhone(phone)
+    if(phone.substring(0, 1) == '0') {
+      phone = phone.substring(1)
+      return fixThPhone(phone)
+    } else {
+      return fixThCityPhone(phone)
+    }
   } else if (phone.substring(0, 3) == '660') {
     phone = phone.substr(0, 2)+phone.substring(3)
     return fixThPhone(phone)
-  }  else {
+  } else if(phone.substring(0, 1) == '0') {
+    phone = phone.substring(1)
+    return fixThPhone(phone)
+  } else {
     return fixThMobilePhone(phone)
   }
 }
