37e73c4988e4b6dc7d79b4b4fd79ff7df33a7888
Yuriy Makov <juramak98@gmail.com>
2019-01-04 19:59:35 +0200

    CRM-3400 changed rules for my

--- src/countries/my.js
+++ src/countries/my.js
@@ -12,7 +12,6 @@ var config = {
 
 var fixMyShortPhone = tools.fixPhoneBuilder(9, 12, config);
 var fixMyLongPhone = tools.fixPhoneBuilder(10, 13, config);
-var fixMyMobilePhone = tools.fixPhoneBuilder(10, 13, config);
 
 var fixMyPhone = function (phone) {
   phone = tools.getSanitizedPhone(phone)
@@ -23,24 +22,11 @@ var fixMyPhone = function (phone) {
   if (localCode > 2)
   console.log(phone)
 
-if (localCode.length === 2) {
-  return fixMyLongPhone(phone)
-} else if (localCode.length === 1) {
-  return fixMyShortPhone(phone)
-} else if (localCode.length > 2) {
-
-}
-/*  if (localCode.length == 2 && phone.length === 13) {
-    return fixMyMobilePhone(phone)
-  }
-  if (localCode.length < 2 && phone.length === 12) {
+  if (localCode.length === 2) {
     return fixMyLongPhone(phone)
-  }
-  if (localCode.length < 2 && phone.length === 11) {
+  } else if (localCode.length === 1) {
     return fixMyShortPhone(phone)
-  } else {
-    return fixMyShortPhone(phone)
-  }*/
+  } 
 }
 
 module.exports = {
