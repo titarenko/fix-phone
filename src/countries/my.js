d9de7ef3b2042d00e9197abb6879eb68626f43bd
Yuriy Makov <juramak98@gmail.com>
2019-01-08 18:54:58 +0200

    CRM-3400

--- src/countries/my.js
+++ src/countries/my.js
@@ -23,26 +23,21 @@ var longPhoneConfig = {
 var fixMyShortPhone = tools.fixPhoneBuilder(9, 12, shortPhoneConfig);
 var fixMyLongPhone = tools.fixPhoneBuilder(10, 13, longPhoneConfig);
 
-
 var fixMyPhone = function (phone) {
   phone = phone.replace(/^(00)/, '')
   phone = phone.replace(/^(0)/, '')
   phone = tools.getSanitizedPhone(phone)
-
   var localCode = tools.getLocalCode(shortPhoneConfig, phone)
   if (/^(11)/.test(localCode)) {
-  localCode = tools.getLocalCode(longPhoneConfig, phone)
-  return fixMyLongPhone(phone)
+    localCode = tools.getLocalCode(longPhoneConfig, phone)
+    return fixMyLongPhone(phone)
   } else {
     return fixMyShortPhone(phone)
   }
-
 } 
 
 function decompose (phone) {
-
   var decompose = tools.decomposeBuilder(fixMyPhone, longPhoneConfig)(phone)
-
   if (/^(11)/.test(decompose.local)){
     return tools.decomposeBuilder(fixMyPhone, longPhoneConfig)(phone)
   } else {
