835533c085996a01f00cd2ca0b8c7890b49d176d
Yuriy Makov <juramak98@gmail.com>
2019-03-11 10:15:57 +0200

    simplified regex on vn

--- src/countries/vn.js
+++ src/countries/vn.js
@@ -14,7 +14,7 @@ var fixVnShortPhone = tools.fixPhoneBuilder(8, 11, config);
 var fixVnLongPhone = tools.fixPhoneBuilder(9, 12, config);
 
 var fixVnPhone = function (phone) {
-  phone = phone.replace(/[^\d\+]/g, '').replace(/^00/g, '').replace(/^0/g, '')
+  phone = phone.replace(/[^\d\+]/g, '').replace(/^0{1,2}/g, '')
   var localCode = tools.getLocalCode(config, phone)
   return localCode.length > 1
     ? fixVnLongPhone(phone)
