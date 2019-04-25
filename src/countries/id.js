9925a27cacf7ca4ee6796acf9169ac9775a1f1f2
Yuriy Makov <juramak98@gmail.com>
2019-04-25 16:02:48 +0300

    CRM-3504 added test cases and sanitization rules for id

--- src/countries/id.js
+++ src/countries/id.js
@@ -10,12 +10,18 @@ var config = {
   },
 }
 
-var fixPhone = tools.fixPhoneBuilder(10, 13, config);
+var fixPhoneShort = tools.fixPhoneBuilder(10, 13, config);
+var fixPhoneLong = tools.fixPhoneBuilder(10, 14, config);
 
 var fixIdPhone = function (phone) {
   phone = phone.replace(/^(0{1,2})/, '')
   phone = phone.replace(/^\+/, '')
-  return fixPhone(phone)
+  var localCodeLength = tools.getLocalCode(config, phone).length
+  if (localCodeLength === 4) {
+    return fixPhoneLong(phone)
+  } else {
+    return fixPhoneShort(phone)
+  }
 }
 
 module.exports = {
