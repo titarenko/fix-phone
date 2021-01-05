cab9023ad95e7d719b228fc626f207d9ce436a5f
MaximVovk <maksemvovk@gmail.com>
2021-01-05 14:52:04 +0200

    expanded rules for country cl

--- tests/countries/cl.js
+++ tests/countries/cl.js
@@ -2,9 +2,11 @@ module.exports = {
   fix: {
     // international format
     '+56000123456' : '+56000123456',
+    '+56976589663' : '+56976589663',
 
     // absense of plus sign
     '56001234567' : '+56001234567',
+    '56976589663' : '+56976589663',
 
     // absesnse of plus sing and country code
     '001234567' : '+56001234567',
@@ -12,6 +14,9 @@ module.exports = {
 
     // absense of country code
     '+001234567': null,
+
+    //with one number of local code
+    '+5621234567': '+5621234567',
 
     // // absense of plus sign, country code and local code
     '1234567': null,
@@ -21,16 +26,30 @@ module.exports = {
     '0331234567' : '+56331234567',
 
     // invalid length
-    '+5600123456' : null,
+    '+560012345' : null,
     '+560012345678' : null,
 
     // sanitization
-    '+ 5  +6 (/00+ 123 - 4567\юв)': '+56001234567'
+    '+ 5  +6 (/00+ 123 - 4567\юв)': '+56001234567',
+
+    //spaces between country code
+    '56 00 1234567': '+56001234567',
+
+    //other sign (dashes,'(', ')' - for example) between country code
+    '56-00-1234567': '+56001234567',
+
+    // country code with one sign ('2' only)
+    '5621234567':'+5621234567',
   },
   decompose: {
     '+56001234567': {
       country: '+56',
       local: '00',
+      phone: '1234567'
+    },
+    '+5621234567': {
+      country: '+56',
+      local: '2',
       phone: '1234567'
     }
   }
