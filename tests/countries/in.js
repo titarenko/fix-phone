e055b8091ded81abad36c7634e33b942bc888b92
Alex <o.olefirenko@everad.com>
2021-01-20 15:32:17 +0200

    add validation for Indian numbers

---
+++ tests/countries/in.js
@@ -0,0 +1,40 @@
+module.exports = {
+  fix: {
+    // international format
+    '+918012345678' : '+918012345678',
+
+    // absense of plus sign
+    '918012345678' : '+918012345678',
+
+    // absesnse of plus sing and country code
+    '8012345678' : '+918012345678',
+
+    // absense of country code
+    '+8012345678': null,
+
+    // // absense of plus sign, country code and local code
+    '12345678': null,
+
+    // zero digit instead of plus sign and country code
+    '08012345678' : '+918012345678',
+
+    // invalid length
+    '+91801234567' : null,
+
+    // sanitization
+    '+ 9  +1 (/80+ 123 - 45678\юв)': '+918012345678',
+
+    //spaces between country code
+    '91 80 12345678': '+918012345678',
+
+    //other sign (dashes,'(', ')' - for example) between country code
+    '91-80-12345678': '+918012345678',
+  },
+  decompose: {
+    '+918012345678': {
+      country: '+91',
+      local: '80',
+      phone: '12345678'
+    }
+  }
+}
